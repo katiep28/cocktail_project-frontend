c8dc047ff497be1fb171a14ba09a5f7eba73fb42
Katie Postle <katiepostle@hotmail.co.uk>
2020-01-17 11:47:14 +0000

    Updates

---
+++ precache-manifest.3f9c6548009fc8d89bdd259fb805261f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef21c427f19e7118084755a1a293e963",
+    "url": "/cocktail_project-frontend/index.html"
+  },
+  {
+    "revision": "974a31bdc85230724c26",
+    "url": "/cocktail_project-frontend/static/css/2.c8b610bc.chunk.css"
+  },
+  {
+    "revision": "e234ed4d95d9f28a757d",
+    "url": "/cocktail_project-frontend/static/css/main.ae74fe99.chunk.css"
+  },
+  {
+    "revision": "974a31bdc85230724c26",
+    "url": "/cocktail_project-frontend/static/js/2.87840941.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/cocktail_project-frontend/static/js/2.87840941.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e234ed4d95d9f28a757d",
+    "url": "/cocktail_project-frontend/static/js/main.bbf1ae65.chunk.js"
+  },
+  {
+    "revision": "37145583ba0b5e5b2d45",
+    "url": "/cocktail_project-frontend/static/js/runtime-main.a9490b42.js"
+  },
+  {
+    "revision": "e3e923cc357208478bfbb47221ffef9d",
+    "url": "/cocktail_project-frontend/static/media/cocktail_skulls_thin.e3e923cc.jpg"
+  },
+  {
+    "revision": "f5e6d352e7e97e49f65630637cd6333a",
+    "url": "/cocktail_project-frontend/static/media/cocktail_summer.f5e6d352.jpg"
+  },
+  {
+    "revision": "5f1a6451075cf073f8e5191f0d8c34ad",
+    "url": "/cocktail_project-frontend/static/media/cocktail_trio_thin.5f1a6451.jpg"
+  },
+  {
+    "revision": "01efec3f86ef259d0f9c2bbebd6b92a6",
+    "url": "/cocktail_project-frontend/static/media/dark_background_for_text.01efec3f.png"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/cocktail_project-frontend/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/cocktail_project-frontend/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/cocktail_project-frontend/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/cocktail_project-frontend/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/cocktail_project-frontend/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "aecbf68a9cd213db8229ad5f72e2eceb",
+    "url": "/cocktail_project-frontend/static/media/med_cocktail_4glasses.aecbf68a.png"
+  },
+  {
+    "revision": "be1e7a72f7556dfe3c249ece02639b70",
+    "url": "/cocktail_project-frontend/static/media/tippl_300_x_300.be1e7a72.png"
+  }
+]);
